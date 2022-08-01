f328c7b79f11f7dc0b3a57ca7d35a2e03258f308
fruktrobot <>
2022-08-01 12:59:10 +0000

    deploy: fruktkartan/fruktkartan@cf9f7ff74dfbe35632f9764bc4e4998dc40d8b9a

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1cf1eec1f817cb3290409d371139c64.js"
+  "/precache-manifest.a52017e732b5637689c090afce0ab0db.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fruktkartan"});
